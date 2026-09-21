981d503649a9ff6f18ab8a2d6b3795af41965205
Salwa Azariouh <salwaazariouh@gmail.com>
2026-09-21 09:39:10 +0200

    feat: refactoring and add input type checks

--- notebook.py
+++ notebook.py
@@ -7,8 +7,9 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
+    import pytest 
 
-    return (mo,)
+    return mo, pytest
 
 
 @app.cell(hide_code=True)
@@ -27,26 +28,38 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    ## Case 2 : first implementation
+    ## Case 2 bis: refactoring of the first implementation
     """)
     return
 
 
 @app.function
 def fibonacci(n):
-    if n==0 :
-        return 0
-    elif n==1 :
-        return 1
-    else :
+    
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("Fibonacci excepts a positive integer")
+        
+    if n>1 :
         return fibonacci(n-1)+fibonacci(n-2)
+        
+    else :
+        return n
 
 
 @app.cell
-def _():
+def test_first_fibo_cases_int_positive():
     assert fibonacci(0)==0
     assert fibonacci(1)==1
     assert fibonacci(2)==1 
+    return
+
+
+@app.cell
+def test_invalid_input_rejection(pytest):
+    with pytest.raises(ValueError):
+        fibonacci(-1)
+    with pytest.raises(ValueError):
+        fibonacci('test')
     return
 
 
