308f45ac3ae3a4103c35c0453471aa206d04839c
Salwa Azariouh <salwaazariouh@gmail.com>
2026-09-25 10:33:00 +0200

    feat: opti  fibonacci with dictionnary memory

--- notebook.py
+++ notebook.py
@@ -63,5 +63,58 @@ def test_invalid_input_rejection(pytest):
     return
 
 
+@app.cell
+def _():
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Case 3 : optimization
+    """)
+    return
+
+
+@app.cell
+def _():
+    # assertions with larger values
+    assert fibonacci(10) == 55
+    assert fibonacci(50) == 12586269025
+    assert fibonacci(100) == 354224848179261915075
+    return
+
+
+@app.cell
+def _():
+    fibo_values = {}
+
+    def fibonacci_opti(n):
+        if not isinstance(n, int) or n < 0:
+            raise ValueError("Fibonacci excepts a positive integer")
+
+        if n in fibo_values:
+            return fibo_values[n]
+
+        elif n > 1:
+            fibo_values[n] = fibonacci_opti(n-1) + fibonacci_opti(n-2)
+            return fibo_values[n]
+        else :
+            return n
+
+    return (fibonacci_opti,)
+
+
+@app.cell
+def _(fibonacci_opti):
+    # assertions with larger values
+    assert fibonacci_opti(10) == 55
+    assert fibonacci_opti(50) == 12586269025
+    assert fibonacci_opti(100) == 354224848179261915075
+
+    print(fibonacci_opti(1000))
+    return
+
+
 if __name__ == "__main__":
     app.run()
